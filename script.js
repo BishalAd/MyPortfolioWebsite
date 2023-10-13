41dacf23aac9aee32321034a2da42bd3d4961111
Bishal Adhikari <87317665+BishalAd@users.noreply.github.com>
2023-10-13 19:00:16 +0545

    browser icon update

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+    $('.navbar .menu li a').click(function () {
+        // applying smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["a BCA Student", "a Basic Programmer", "Learning Web Developement", "Working On Company", "Basic Video Editor"],
+        strings: ["a BCA Student", "a Basic Programmer", "Learning Web Development", "Working On Company", "Basic Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["a BCA Student", "a Basic Programmer", "Learning Web Developement", "Working On Company", "Basic Video Editor"],
+        strings: ["a BCA Student", "a Basic Programmer", "Learning Web Development", "Working On Company", "Basic Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -56,18 +56,39 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
+    });
+
+    // Handle form submission
+    $('#contact-form').submit(function (e) {
+        e.preventDefault(); // Prevent the default form submission
+
+        // Get form data
+        var name = $('#contact-form input[name="name"]').val();
+        var email = $('#contact-form input[name="email"]').val();
+        var subject = $('#contact-form input[name="subject"]').val();
+        var message = $('#contact-form textarea[name="message"]').val();
+
+        // Construct the mailto link
+        var mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+
+        // Open the email client in a new tab
+        window.open(mailtoLink);
+
+        // You can also add the AJAX code here to send the message to the server if needed.
+
+        return false; // Prevent the form from submitting again
     });
 });
--- send_email.php
+++ send_email.php
@@ -1,5 +1,5 @@
 <?php
-require 'path/to/PHPMailerAutoload.php'; // Make sure to include the PHPMailer library
+require 'C:\Program Files\PHPMailer\test\PHPMailer'; // Make sure to include the PHPMailer library
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -7,22 +7,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    $toEmail = "onlinesewatest@gmail.com"; // Replace with your Gmail address
-    $smtpUsername = "onlinesewatest@gmail.com"; // Your Gmail username
-    $smtpPassword = "Online Sewa Test @123"; // Your Gmail password
-
     // Create a PHPMailer object
     $mail = new PHPMailer;
+
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->Port = 587; // Use 465 if you're having issues with 587
     $mail->SMTPSecure = 'tls';
     $mail->SMTPAuth = true;
-    $mail->Username = $smtpUsername;
-    $mail->Password = $smtpPassword;
+
+    $mail->Username = 'onlinesewatest@gmail.com';
+
+    $mail->Password = 'Online Sewa Test @123';
 
     $mail->setFrom($email, $name);
-    $mail->addAddress($toEmail);
+    $mail->addAddress('mmeow6844@gmail.com'); 
     $mail->isHTML(true);
     $mail->Subject = $subject;
     $mail->Body = $message;
@@ -31,6 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "success"; // You can handle the success response on the client-side
     } else {
         echo "error"; // You can handle the error response on the client-side
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
 }
 ?>
